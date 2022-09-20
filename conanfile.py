0f0f67a3fca5931ebbb25823c95890b813878c09
Chake96 <carsonhake@gmail.com>
2022-09-20 10:03:16 -0400

    added libuvc ontop of libusb

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,12 @@ class CerberusPkg(ConanFile):
     settings = {
         "build_type",
     }
-    requires = ["abseil/20211102.0", "gtest/cci.20210126", "boost/1.79.0", 
-    "ftxui/3.0.0", "onetbb/2021.3.0", "si/[>=2.5.0]", "libusb/[>=1.0.26]", 
-    "spdlog/[>=1.10.0]", "libsystemd/251.4","fmt/[>=9.1.0]","cli11/2.2.0"]
+    requires = [
+        "abseil/20211102.0", "gtest/cci.20210126", "boost/1.79.0", "ftxui/3.0.0", 
+        "onetbb/2021.3.0", "si/[>=2.5.0]", "spdlog/[>=1.10.0]","libsystemd/251.4",
+        "fmt/[>=9.1.0]","cli11/2.2.0", "opencv/[>=4.5.0]", "libcap/[>=2.62]",
+        "libusb/[>=1.0.26]"
+    ]
 
     def layout(self):
         cmake_layout(self)
